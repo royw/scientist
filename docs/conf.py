adf492864dfd2d68a3e10fd441276c849b47579f
royw <roy@wright.org>
2016-02-03 19:39:43 -0600

    corrected project_description string cause it has an apostrophe

--- docs/conf.py
+++ docs/conf.py
@@ -84,13 +84,14 @@ project_name = u'Science'
 project_title = u'{project_name} Documentation'.format(project_name=project_name)
 project_author = u'Roy Wright'
 project_author_email = u'roy.oti.wright@hpe.com'
-project_description = u'Port of python of github's ruby scientist.'
+project_description = u"Port of python of github's ruby scientist."
 project_package = u'scientist'
 project_bugzilla_url = u'https://bugzilla.austin.hp.com'
 
 # -- hack that modifies autodoc to skip unwanted class methods ----------------
 
-# noinspection PyDocstring,PyUnusedLocal
+
+# noinspection PyDocstring,PyUnusedLocal,PyIncorrectDocstring
 def skipUnwanted(app, what, name, obj, skip, options):
     """Skip __dict__, __doc__ and __abstractmethods__ entries"""
     if what == "class" and name in ["__dict__", "__doc__", "__abstractmethods__"]:
